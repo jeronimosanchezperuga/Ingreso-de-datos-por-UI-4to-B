7a3fc2008684a2b50d057ca2c7567511b7120811
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-10 12:22:14 -0300

    Initial commit

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUserInput; 
+
+    public void ShowGreetings()
+    {
+        Debug.Log(int.Parse(txtUserInput.text) * 2);
+    }
+}
---
+++ Assets/Instantiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public int cloneAmount;
+    public Text txtCloneAmount;
+    GameObject clon;
+
+    public void MultiInstantiate()
+    {
+        cloneAmount = int.Parse(txtCloneAmount.text);
+        for (int i = 0; i < cloneAmount; i++)
+        {
+            clon = Instantiate(objectToClone);
+            Destroy(clon,1);
+        }
+    }
+}
